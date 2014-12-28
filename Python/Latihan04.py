51731b2f5415f4a35e30e0dec960f40c428c5ae2
Herman <herman.wahyudi02@gmail.com>
2014-12-28 11:36:51 +0700

    Latihan python sess 1

--- Python/Latihan04.py
+++ Python/Latihan04.py
@@ -6,3 +6,29 @@ str = "herman"[3:]
 print(str)
 
 s = "sdfs"+ "wqeqw"
+
+k = lambda x="8", u="0": x + u
+print(k("","9"))
+
+def sebuah_fungsi():
+	str1 = "Her"
+	f = lambda x: str1 + " " + x
+	return f
+
+act = sebuah_fungsi()
+o = act("hh")
+print(act)
+
+#raise ValueError("Hai")
+
+with open("file.txt") as file:
+	for line in file:
+		print(line)
+
+def reverse(str):
+	for idx in range(len(str)-1, -1, -1):
+		print(str[idx])
+		#yield str[idx]
+		
+
+reverse("Her")
