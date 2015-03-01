7b5ab2d47f6f350baed787758322471e5d3a64f4
Herman <herman.wahyudi02@gmail.com>
2015-03-01 18:52:13 +0700

    Update python modul

---
+++ Python/LatihanRegex.py
@@ -0,0 +1,15 @@
+import re
+
+class LatihanRegex:
+	
+	def __init__(self):
+		return self
+
+	def pattern(self):
+
+	def check(self):
+		return 0
+
+if __name__ == "__main__":
+	o = LatihanRegex()
+	print(o)
